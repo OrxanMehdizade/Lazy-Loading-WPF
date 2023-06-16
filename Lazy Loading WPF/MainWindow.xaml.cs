9e5e2da4513fe44f8275fc381e15e477216c3ca4
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-06-16 17:03:26 +0400

    Add project files.

---
+++ Lazy Loading WPF/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lazy_Loading_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Book> GetBooks { get; } = new ObservableCollection<Book>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillTableComboBox();
+            DataContext = this;
+        }
+
+        private void FillTableComboBox()
+        {
+            using (var db = new LibraryContext())
+            {
+                var tableNames = db.Model.GetEntityTypes()
+                    .Select(c => c.GetTableName())
+                    .ToList();
+
+                ComboClass.Items.Clear();
+
+                foreach (var tableName in tableNames)
+                {
+                    ComboClass.Items.Add(new ComboBoxItem { Content = tableName });
+                }
+            }
+        }
+
+        private void ComboClass_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            using (var database = new LibraryContext())
+            {
+                if (ComboClass.SelectedItem is ComboBoxItem selectedItem)
+                {
+                    string selectedTable = selectedItem.Content.ToString();
+
+                    ComboColumns.Items.Clear();
+
+                    if (selectedTable == "Authors")
+                    {
+                        var authors = database.Authors.ToList();
+                        authors.ForEach(a => ComboColumns.Items.Add($"{a.FirstName} {a.LastName}"));
+                    }
+                    else if (selectedTable == "Themes")
+                    {
+                        var themes = database.Themes.ToList();
+                        themes.ForEach(t => ComboColumns.Items.Add($"{t.Name}"));
+                    }
+                    else if (selectedTable == "Categories")
+                    {
+                        var categories = database.Categories.ToList();
+                        categories.ForEach(c => ComboColumns.Items.Add($"{c.Name}"));
+                    }
+                }
+            }
+        }
+
+        private void ComboColumns_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            using (var database = new LibraryContext())
+            {
+                if (ComboClass.SelectedItem is ComboBoxItem selectedItem)
+                {
+                    string selectedTable = selectedItem.Content.ToString();
+                    GetBooks.Clear();
+
+                    if (selectedTable == "Authors" && ComboColumns.SelectedItem is string author)
+                    {
+                        var authorBooks = database.Books
+                            .Include(b => b.Authors)
+                            .Where(b => $"{b.Authors.FirstName} {b.Author.LastName}" == author)
+                            .ToList();
+                        authorBooks.ForEach(s => GetBooks.Add(s));
+                    }
+                    else if (selectedTable == "Themes" && ComboColumns.SelectedItem is string theme)
+                    {
+                        var themeBooks = database.Books
+                            .Include(b => b.Theme)
+                            .Where(b => $"{b.Theme.Name}" == theme)
+                            .ToList();
+                        themeBooks.ForEach(s => GetBooks.Add(s));
+                    }
+                    else if (selectedTable == "Categories" && ComboColumns.SelectedItem is string category)
+                    {
+                        var categoryBooks = database.Books
+                            .Include(b => b.Category)
+                            .Where(b => $"{b.Category.Name}" == category)
+                            .ToList();
+                        categoryBooks.ForEach(s => GetBooks.Add(s));
+                    }
+                }
+            }
+        }
+    }
+
+}
+
---
+++ Lazy Loading WPF/Theme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lazy_Loading_WPF;
+
+public partial class Theme
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
